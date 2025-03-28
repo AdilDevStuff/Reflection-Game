37ccfdab5169eaa22a3550c4d4ad5197ba5c97f5
AdilDevStuff <adilshafiq784@gmail.com>
2025-03-29 00:54:31 +0500

    Improved Reflection System

--- scripts/light_ray_manager.gd
+++ scripts/light_ray_manager.gd
@@ -1,75 +1,63 @@
 extends Node2D
 
-var rot: float = 0.0
-var bounces: int = 3
-var lasers: Array = []
-var max_cast_to: Vector2
-
-const MAX_LENGTH: float = 2000.0
+@export var max_bounces: int = 3
 
 @export var line: Line2D
-@export var light_ray: RayCast2D
-@export var light_source: CharacterBody2D
-@export var ray_origin_marker: Marker2D
+@export var ray_origin: Marker2D
+@export var raycast: RayCast2D
 
-func _ready() -> void:
-	setup_lasers()
-	setup_initial_state()
+var points: Array = []
 
-func setup_lasers() -> void:
-	lasers.append(light_ray)
-	for i in range(bounces):
-		var raycast = light_ray.duplicate()
-		raycast.enabled = false
-		add_child(raycast)
-		lasers.append(raycast)
+func _process(_delta):
+	calculate_light_path()
 
-func setup_initial_state() -> void:
-	max_cast_to = Vector2(MAX_LENGTH, 0).rotated(rot)
-	light_ray.target_position = max_cast_to
-	line.top_level = true
-
-func _process(_delta: float) -> void:
-	reset_line()
-	update_lasers()
-
-func reset_line() -> void:
-	line.clear_points()
-	line.add_point(ray_origin_marker.global_position)
-	max_cast_to = Vector2(MAX_LENGTH, 0).rotated(rot)
-
-func update_lasers() -> void:
-	for idx in lasers.size():
-		var raycast = lasers[idx]
-		var raycast_collision = raycast.get_collision_point()
-
-		setup_raycast(raycast, idx)
-
-		if raycast.is_colliding():
-			handle_collision(raycast, raycast_collision, idx)
+func calculate_light_path():
+	points = [ray_origin.global_position]
+	
+	var start = ray_origin.global_position
+	var direction = Vector2.RIGHT.rotated(rotation)
+	
+	for _i in range(max_bounces):
+		var result = cast_ray(start, direction)
+		
+		if result:
+			var collision_point = result.position
+			var collider = result.collider
+			var normal = result.normal
+			
+			points.append(collision_point)
+			
+			if collider.is_in_group("Mirror"):
+				print("%.2f" % direction.dot(normal))
+				if direction.dot(normal) > 0:
+					break  
+				
+				direction = direction.bounce(normal)
+				start = collision_point + direction * 1
+			else:
+				# Collider is blocking
+				break
 		else:
-			line.add_point(raycast.global_position + max_cast_to)
+			points.append(start + direction * 2000)
 			break
+	update_line(points)
 
-func setup_raycast(raycast: RayCast2D, idx: int) -> void:
-	raycast.target_position = max_cast_to
-	if idx == 0:
-		raycast.global_position = ray_origin_marker.global_position
 
-func handle_collision(raycast: RayCast2D, raycast_collision: Vector2, idx: int) -> void:
-	var collider = raycast.get_collider()
-	if collider.is_in_group("Target"):
-		# TODO: If raycast collides with target, then level is completed.
-		pass
-	elif collider.is_in_group("Mirror"):
-		handle_mirror_collision(raycast_collision, raycast.get_collision_normal(), idx)
+func cast_ray(start: Vector2, direction: Vector2):
+	raycast.global_position = start
+	raycast.target_position = direction * 2000
+	raycast.force_raycast_update()
 
-func handle_mirror_collision(collision_point: Vector2, normal: Vector2, idx: int) -> void:
-	line.add_point(collision_point)
-	max_cast_to = max_cast_to.bounce(normal)
-	if idx < lasers.size() - 1:
-		lasers[idx + 1].enabled = true
-		lasers[idx + 1].global_position = collision_point + max_cast_to.normalized()
-	if idx == lasers.size()-1: pass
-		# TODO: Add particle effect here
-		# $End.global_position = raycastcollision
+	if raycast.is_colliding():
+		return {
+			"position": raycast.get_collision_point(),
+			"normal": raycast.get_collision_normal(),
+			"collider": raycast.get_collider()
+		}
+	
+	return null
+
+func update_line(points: Array):
+	line.clear_points()
+	for point in points:
+		line.add_point(to_local(point))
